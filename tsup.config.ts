c7a7c7c818b7733280afd9b391c2e1fd0c3aac29
chengchongzhen <15939054361@163.com>
2025-03-10 20:08:08 +0800

    chore: 调整构建配置为 CommonJS 模块

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['main.ts'],
-  format: ['esm'],
+  format: ['cjs'],
   clean: true,
   dts: true,
   outDir: 'dist',
@@ -10,4 +10,10 @@ export default defineConfig({
   inject: ['types/env.d.ts'],
   minify: false,
   sourcemap: true,
+  noExternal: ['consola'],
+  banner: {
+    js: "#!/usr/bin/env node",
+  },
+  platform: 'node',
+  target: 'node16',
 }); 
